a061509f99804947f93521f803e4a37354973236
BekhzodFozilov <bekhzodfozilov@gmail.com>
2019-07-24 16:06:36 +0500

    Change code in Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,6 +11,10 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World !!!");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadKey();
         }
     }
